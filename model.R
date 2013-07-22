e59915bd947674cc561674f1ed2fe02130480732
Lei Gong <lgong0715@gmail.com>
2013-07-21 22:27:59 -0700

    add posterior analysis

--- model.R
+++ model.R
@@ -87,12 +87,11 @@ while(1){
 	for(t in (n-1):2){
 		prob<- c(prod(mass[t, S_cur[t+1]-1])*(1-prod(P_cur[S_cur[t+1]-1])), mass[t, S_cur[t+1]]*P_cur[S_cur[t+1]])
 		prob<- prob/sum(prob)	# probability for pick from S_cur[t+1]-1:S_cur[t+1]
-		print(prob)
 		S_cur[t]<- sample((S_cur[t+1]-1):S_cur[t+1], 1, prob = prob)
 	}
 	
 	# update P
-	count<- ftable(S_cur)-1	#n_ii
+	count<- table(S_cur)-1	#n_ii
 	for(i in 1:m){
 		x1<- rgamma(1, a+count[i], 1)
 		x2<- rgamma(1, b+1, 1)
@@ -127,5 +126,105 @@ while(1){
 		}
 	}
 }
+
+
+## posterior analysis ##
+# total simulation effort
+G<- dim(P)[1]
+
+# posterior mean
+P_star<- apply(P, 2, mean)
+theta_star<- apply(theta, 2, mean)
+
+# likelihood at star
+y_like<- array(0, n)
+mass<- array(0, c(n, m+1))
+mass[1, 1]<- 1
+y_like[1]<- ppois(y[1], theta_star[1])
+for(t in 2:n){
+	mass[t, 1]<- 1	# if k == 1 then k-1 == 0
+	for(k in 2:(m+1)){
+		mass[t, k]<- 1/(1+eq6(t, k-1, P_star, mass)*ppois(y[t], theta_star[k-1])/(eq6(t, k, P_star, mass)*ppois(y[t], theta_star[k])))
+	}
+	mass[t,]<- mass[t,]/sum(mass[t,])
+	for(k in 1:(m+1)){
+		y_like[t]<- y_like[t]+ppois(y[t], theta_star[k])*eq6(t, k, P_star, mass)
+	}
+}
+ln_y_like<- sum(log(y_like))	# likelihood function
+
+#marginal likelihood
+time<- array(NA, c(G, m))	# change-points
+theta_post<- array(NA, c(G, m+1))
+S_post<- array(NA, c(G, n))
+P_post<- array(NA, c(G, m))
+P_update<- P_star
+S_plot<- array(0, c(G, n, m+1))
+for(i in 1:G){
+	# update time
+	time[i,]<- cumsum(table(S[i,]))[-(m+1)]
+	
+	# update theta_post
+	for(k in 1:(m+1)){
+		theta_post[i, k]<- pgamma(theta_star[k], m+1+sum(y[S[i,] == k]), table(S[i,])[k]+1)
+	}
+	
+	# update S_post
+	mass<- array(0, c(n, m+1))
+	mass[1, 1]<- 1
+	for(t in 2:n){
+		mass[t, 1]<- 1	# if k == 1 then k-1 == 0
+		for(k in 2:(m+1)){
+			mass[t, k]<- 1/(1+eq6(t, k-1, P_update, mass)*ppois(y[t], theta_star[k-1])/(eq6(t, k, P_update, mass)*ppois(y[t], theta_star[k])))
+		}
+		mass[t,]<- mass[t,]/sum(mass[t,])
+	}
+	S_post[i, n]<- m+1
+	S_post[i, 1]<- 1
+	for(t in (n-1):2){
+		prob<- c(prod(mass[t, S_post[i, t+1]-1])*(1-prod(P_update[S_post[i, t+1]-1])), mass[t, S_post[i, t+1]]*P_update[S_post[i, t+1]])
+		prob<- prob/sum(prob)	# probability for pick from S_cur[t+1]-1:S_cur[t+1]
+		S_post[i, t]<- sample((S_post[i, t+1]-1):S_post[i, t+1], 1, prob = prob)
+	}
+	count_post<- table(S_post[i,])-1	#n_ii
+	for(k in 1:m){
+		x1<- rgamma(1, a+count_post[k], 1)
+		x2<- rgamma(1, b+1, 1)
+		P_update[k]<- x1/(x1+x2)
+		P_post[i, k]<- gamma(a+b+count_post[k]+1)/(gamma(a+count_post[k])*gamma(b+1))*P_star[k]^(a+count_post[k]-1)*(1-P_star[k])^(b+1-1)
+	}
+	
+	# update S_plot
+	mass<- array(0, c(n, m+1))
+	mass[1, 1]<- 1
+	for(t in 2:n){
+		mass[t, 1]<- 1	# if k == 1 then k-1 == 0
+		for(k in 2:(m+1)){
+			mass[t, k]<- 1/(1+eq6(t, k-1, P[i,], mass)*ppois(y[t], theta[i, k-1])/(eq6(t, k, P[i,], mass)*ppois(y[t], theta[i, k])))
+		}
+		mass[t,]<- mass[t,]/sum(mass[t,])
+	}
+	for(t in 1:n){
+		for(k in 1:(m+1)){
+			S_plot[i, t, k]<- eq6(t, k, P[i,], mass)
+		}
+	}
+}
+
+ln_theta_post_den<- log(mean(apply(theta_post, 1, prod)))
+ln_P_post_den<- log(mean(apply(P_post, 1, prod)))
+ln_theta_den<- sum(log(apply(as.array(theta_star), 1, pgamma, m+1, 1)))
+ln_P_den<- sum(log(apply(as.array(P_star), 1, pbeta, a, b)))
+
+# bayes factor
+ln_bayes<- ln_y_den+ln_theta_den+ln_P_den - ln_theta_post_den-ln_P_post_den
+write.table(ln_bayes, file = paste(m, "bayes.txt", sep = ''))
+
+# plots
+S_plots<- apply(S_plot, c(2,3), mean)
+write.table(S_plots, file = paste(m, "Splots.txt", sep = ''))
+
+
+
 
 	
